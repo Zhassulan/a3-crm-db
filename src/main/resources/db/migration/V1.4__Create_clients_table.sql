9c2688012f84470bb86830e7ba015c6c1d77fffc
Zhassulan Tokbayev <ztokbayev@gmail.com>
2022-05-08 14:21:34 +0600

    flyway

--- src/main/resources/db/migration/V1.4__Create_clients_table.sql
+++ src/main/resources/db/migration/V1.4__Create_clients_table.sql
@@ -2,6 +2,7 @@ create table if not exists clients
 (
     id integer PRIMARY KEY,
     client_type_id integer not null REFERENCES client_types (id),
+    address_id integer not null REFERENCES addresses (id),
     title varchar(255) not null ,
     created date not null default now(),
     modified date not null default now()
